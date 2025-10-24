9f2675ce300ef8bd215774f2fc6a40945ebf56e2
songdayoung <tjs2446@gmail.com>
2025-10-24 14:23:01 +0900

    [feature] - 1차 백엔드 환경 세팅 적용

---
+++ backend/src/main/java/kr/co/mongmate/MongmateApplication.java
@@ -0,0 +1,13 @@
+package kr.co.mongmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongmateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/kr/co/mongmate/MongmateApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.mongmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
